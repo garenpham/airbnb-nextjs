0f9a8107a431150335639ef6ff352342dcf4bd03
Pham Minh Tan <tanphamminh2002@gmail.com>
2023-01-08 07:25:22 -0800

    mapbox

--- next.config.js
+++ next.config.js
@@ -8,4 +8,8 @@ module.exports = {
 			'jsonkeeper.com',
 		],
 	},
+	env: {
+		mapbox_key:
+			'pk.eyJ1IjoicGhhbW1pbmh0YW4iLCJhIjoiY2w2cmNnbzlrMTJmaDNlbnNzczNmYWFpdCJ9.wz3gVBNdBv5c5nQG1dfD-Q',
+	},
 }
