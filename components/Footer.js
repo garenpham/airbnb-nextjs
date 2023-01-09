c853b72e157d2b5e95f8f7287243b8f45fdfccff
Pham Minh Tan <tanphamminh2002@gmail.com>
2023-01-09 14:39:36 -0800

    highlight footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,45 +2,47 @@ const style = {
 	wrapper: `grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600`,
 	container: `space-y-4 text-xs-gray-800`,
 	title: `font-bold`,
-}
+	content: `hover:underline`,
+	// hover:underline
+};
 
 function Footer() {
 	return (
 		<div className={style.wrapper}>
 			<div className={style.container}>
 				<h5 className={style.title}>ABOUT</h5>
-				<p>How Airbnb works</p>
-				<p>Newsroom</p>
-				<p>Investors</p>
-				<p>Airbnb Plus</p>
-				<p>Airbnb Luxe</p>
+				<p className={style.content}>How Airbnb works</p>
+				<p className={style.content}>Newsroom</p>
+				<p className={style.content}>Investors</p>
+				<p className={style.content}>Airbnb Plus</p>
+				<p className={style.content}>Airbnb Luxe</p>
 			</div>
 			<div className={style.container}>
 				<h5 className={style.title}>COMMUNITY</h5>
-				<p>Accessibility</p>
-				<p>This is not a real site</p>
-				<p>Its a pretty awesome clone</p>
-				<p>With tailwindCSS and Nextjs</p>
-				<p>Inspired by PapaReact</p>
+				<p className={style.content}>Accessibility</p>
+				<p className={style.content}>This is not a real site</p>
+				<p className={style.content}>Its a pretty awesome clone</p>
+				<p className={style.content}>With tailwindCSS and Nextjs</p>
+				<p className={style.content}>Inspired by PapaReact</p>
 			</div>
 			<div className={style.container}>
 				<h5 className={style.title}>HOST</h5>
-				<p>Pham Minh Tan</p>
-				<p>Presents</p>
-				<p>Airbnb Clone</p>
-				<p>With Nextjs</p>
-				<p>Visit my Github</p>
+				<p className={style.content}>Pham Minh Tan</p>
+				<p className={style.content}>Presents</p>
+				<p className={style.content}>Airbnb Clone</p>
+				<p className={style.content}>With Nextjs</p>
+				<p className={style.content}>Visit my Github</p>
 			</div>
 			<div className={style.container}>
 				<h5 className={style.title}>SUPPORT</h5>
-				<p>Help Centre</p>
-				<p>Trust & Safety</p>
-				<p>Contact Us</p>
-				<p>Easter Eggs</p>
-				<p>Happy Coding!</p>
+				<p className={style.content}>Help Centre</p>
+				<p className={style.content}>Trust & Safety</p>
+				<p className={style.content}>Contact Us</p>
+				<p className={style.content}>Easter Eggs</p>
+				<p className={style.content}>Happy Coding!</p>
 			</div>
 		</div>
-	)
+	);
 }
 
-export default Footer
+export default Footer;
